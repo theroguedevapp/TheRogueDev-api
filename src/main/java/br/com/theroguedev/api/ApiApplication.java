a9f4afa3ed4f3f01e59afefcdbc4cdf47dbba495
betgrave <betgrave@gmail.com>
2025-10-14 20:04:10 -0300

    feat: implements swagger

--- src/main/java/br/com/theroguedev/api/ApiApplication.java
+++ src/main/java/br/com/theroguedev/api/ApiApplication.java
@@ -2,7 +2,10 @@ package br.com.theroguedev.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+
+@EnableFeignClients
 @SpringBootApplication
 public class ApiApplication {
 
